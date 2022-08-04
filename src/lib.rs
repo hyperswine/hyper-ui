dafb222496a3ef2743837a905fc6ac800ab9f278
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-04 18:24:10 +1000

    Update lib api

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,76 @@
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+
 use dioxus::prelude::*;
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum FlexDir {
-    Row,
-    Column,
-}
+// MODULES
+
+pub mod modal;
+pub mod progress_bar;
+
+// Useful Constants
+
+pub type Color<'a> = &'a str;
+
+pub const Row: &'static str = "row";
+pub const Col: &'static str = "column";
+
+// Colors
+
+pub const Black: &'static str = "0x000";
+pub const White: &'static str = "0xFFF";
+
+// Components
 
 #[derive(Props)]
 pub struct FlexProps<'a> {
-    flex: FlexDir,
-    children: Element<'a>
+    flex: &'a str,
+    children: Element<'a>,
 }
 
 pub fn Flex<'a>(cx: Scope<'a, FlexProps<'a>>) -> Element {
     cx.render(rsx!(div {
-        flex_direction: "column",
-        padding: "2",
+        flex_direction: "{cx.props.flex}",
+        display: "flex",
         &cx.props.children
     }))
 }
+
+#[derive(Props)]
+pub struct BoxProps<'a> {
+    children: Element<'a>,
+}
+
+pub fn Box<'a>(cx: Scope<'a, BoxProps<'a>>) -> Element {
+    cx.render(rsx!(div {
+        &cx.props.children
+    }))
+}
+
+// Animations
+
+// Mostly wrappers around css
+
+// MACROS
+
+#[macro_export]
+macro_rules! render {
+    ($input:tt) => {
+        cx.render(rsx!($input))
+    };
+}
+
+// maybe make a derive macro that does this, in a macros lib
+
+#[macro_export]
+macro_rules! component {
+    ($name:ident,$body:tt) => {
+        #[derive(Props)]
+        pub struct $ident<'a> {
+            $body,
+            children: Element<'a>,
+        }
+    };
+}
+
+// maybe a derive macro for the actual component itself
