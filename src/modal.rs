1e5487ec0c225e9fa39633634bf72c37fc1da378
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-04 18:48:40 +1000

    Update modal to use render macro

--- src/modal.rs
+++ src/modal.rs
@@ -1,11 +1,16 @@
-use crate::render;
-use dioxus::prelude::*;
+use crate::{render, render_plain};
+use dioxus::prelude::{dioxus_elements::div, *};
 
 #[derive(Props)]
 pub struct Modal<'a> {
     children: Element<'a>,
 }
 
+/// The modal
 pub fn Modal<'a>(cx: Scope<'a, Modal<'a>>) -> Element {
-    cx.render(rsx!(div {}))
+    // cx.render(rsx!(div {}))
+    // render_plain!(cx, div {"Hi"})
+    render!(cx, { "Hi" })
 }
+
+// maybe for now, always render a div?
