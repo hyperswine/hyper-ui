64db06c551e467420c54b1362c0603d8681d24a0
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-04 17:12:16 +1000

    Add flex

--- examples/main.rs
+++ examples/main.rs
@@ -1,3 +1,34 @@
+use dioxus::{events::onclick, prelude::*};
+use hyper_ui::{Flex, FlexDir};
+
 fn main() {
-    println!("Hello, Hyper UI!")
+    dioxus::desktop::launch_cfg(app, |cfg| {
+        cfg.with_window(|w| w.with_title("HyperUI Demo").with_decorations(false))
+    });
+}
+
+fn app(cx: Scope) -> Element {
+    let window = dioxus::desktop::use_window(&cx);
+    let fullscreen = use_state(&cx, || false);
+    let always_on_top = use_state(&cx, || false);
+    let decorations = use_state(&cx, || true);
+
+    window.set_resizable(true);
+    // window.set_title("Hyper UI");
+
+    cx.render(rsx!(
+        div {
+            onmousedown: move |_| window.drag(),
+            height: "100%",
+            width: "100%",
+            "Header",
+        }
+        div {
+            "This is Hyper UI"
+        }
+        Flex {
+            flex: FlexDir::Row,
+            "This is a flex container"
+        }
+    ))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use dioxus::prelude::*;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum FlexDir {
+    Row,
+    Column,
+}
+
+#[derive(Props)]
+pub struct FlexProps<'a> {
+    flex: FlexDir,
+    children: Element<'a>
+}
+
+pub fn Flex<'a>(cx: Scope<'a, FlexProps<'a>>) -> Element {
+    cx.render(rsx!(div {
+        flex_direction: "column",
+        padding: "2",
+        &cx.props.children
+    }))
 }
