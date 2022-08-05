1fe95a61ca8edd22695beba106496f64f7c84fb7
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-06 09:34:07 +1000

    Update macros proc

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,9 +2,37 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
+// I dont think you need the actual derive trait in the proc lib itself
+// hyper props have children only if you insert the 'children' keyword
+// at the end of the struct as a psuedo field instruction
+// hyper props cant have new, debug, etc cause its not a proper struct
+// which uhh, is kind of a problem if you ask me
+// why cant you derive debug again? maybe just impl it then
+
 #[proc_macro_derive(HyperProps)]
 pub fn hyper_props(body: TokenStream) -> TokenStream {
-    body
+    let is_pub = true;
+    // Identifier
+    let name = "";
+    // fields of the struct. Note if you
+    let body = "";
+    let has_children = true;
+
+    quote!(
+        #[derive(Props)]
+        // pub?
+        struct #name {
+            #body
+            children: Element<'a>,
+        }
+
+        impl Debug for #name {
+            pub fn debug(&self) {
+
+            }
+        }
+    )
+    .into()
 }
 
 #[proc_macro]
